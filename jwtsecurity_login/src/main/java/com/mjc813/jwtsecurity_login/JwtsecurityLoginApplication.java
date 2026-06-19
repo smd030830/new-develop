f1958c8ddce4b22893e6f1d119592f9c8f46e5e8
smd030830 <smd030830@gmail.com>
2026-06-19 17:59:00 +0900

    jwtsecurity 공부 시작

---
+++ jwtsecurity_login/src/main/java/com/mjc813/jwtsecurity_login/JwtsecurityLoginApplication.java
@@ -0,0 +1,13 @@
+package com.mjc813.jwtsecurity_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtsecurityLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtsecurityLoginApplication.class, args);
+    }
+
+}
---
+++ jwtsecurity_login/src/test/java/com/mjc813/jwtsecurity_login/JwtsecurityLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjc813.jwtsecurity_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtsecurityLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
